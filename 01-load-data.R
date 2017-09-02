fbc658f8c44d35163a09579976539c72cb49c6e1
Keith Hultman <keith.hultman@elmhurst.edu>
2017-09-02 11:12:40 -0500

    Load SAS files and save as Rdata files

--- 01-load-data.R
+++ 01-load-data.R
@@ -1,2 +1,13 @@
 # Open SAS and txt data files and save as RData files
 
+library(haven)
+
+appeal <- read_sas("./data/appeal.sas7bdat")
+donor <- read_sas("./data/donor.sas7bdat")
+trans <- read_sas("./data/trans.sas7bdat")
+source <- read_sas("./data/source.sas7bdat")
+
+save(appeal, file = "./data/appeal.Rdata")
+save(donor, file = "./data/donor.Rdata")
+save(trans, file = "./trans.Rdata")
+save(source, file = "./source.Rdata")
